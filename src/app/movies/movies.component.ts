765980648b27102a1a7cabada214188026891555
ozgur oguz <ozgur.oguz@hotmail.com>
2020-03-02 21:47:37 +0300

    Component 1:Generating Component

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component
+({
+    selector: 'movies'  , //#movies-> id .movies-> class  movies ->> etiket
+    //template: '<h2>Movies</h2>'
+    templateUrl: 'movies.component.html'
+ })
+export class MoviesComponent
+{
+
+}
